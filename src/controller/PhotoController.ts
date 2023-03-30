d12d856cd3f51c3fe9e420d30eab54f6ef31ac83
Paul Bowyer <xblue@protonmail.ch>
2023-03-30 09:23:08 +0300

    chore: update all crud methods photo controller

--- src/controller/PhotoController.ts
+++ src/controller/PhotoController.ts
@@ -48,4 +48,71 @@ export class PhotoController {
       res.status(404).send('Your request could not be processed!')
     }
   }
+
+  static getAllPhotos = async (
+    req: Request,
+    res: Response
+  ): Response<Photo[]> => {
+    try {
+      const ur = AppDataSource.getRepository(User)
+      // get user.id
+      const userId = req.user.userId
+      const user = await ur.findOne({
+        relations: ['photos'],
+        where: {
+          id: userId, // where the id is of the author. expecting three 3 photos connected to this single author
+        },
+      })
+
+      return res.status(200).send(user)
+    } catch (error) {
+      res.status(404).send('Your request could not be processed!')
+    }
+  }
+
+  static update = async (req: Request, res: Response): Response<any> => {
+    try {
+      const ur = AppDataSource.getRepository(User)
+      const userId = req.user.userId // Get the user ID from previous midleware
+
+      const user = await ur.findOne({
+        relations: ['photos'],
+        where: {
+          id: userId,
+        },
+      })
+
+      // Retrieve the Photo entity with ID 2 from the user's photos array
+      const updatedPhoto = user.photos.find(
+        (photo) => photo.id === Number(req.params.id)
+      ) // todo extract req.body.id from req.params
+      updatedPhoto.description = req.body.description
+
+      // Save the updated Photo entity to the database
+      await ur.save(user)
+      res.status(200).send(updatedPhoto)
+    } catch (error) {
+      res.status(404).send('Your request could not be processed!')
+    }
+  }
+
+  static delete = async (req: Request, res: Response): Response<any> => {
+    try {
+      const pr = AppDataSource.getRepository(Photo)
+      // Find the photo we want to delete
+      const photo = await pr.findOneOrFail({
+        relations: ['user'],
+        where: {
+          id: req.params.photoId,
+        },
+      })
+
+      // Delete the photo from the DB
+      await pr.delete(photo)
+
+      return res.status(200).send('OK!')
+    } catch (error) {
+      return res.status(404).send('Photo was not found!')
+    }
+  }
 }
