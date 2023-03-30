f9c25bae44169d504edc5f45c6681b424f691439
Paul Bowyer <xblue@protonmail.ch>
2023-03-30 13:54:55 +0300

    chore: add patch route and method

--- src/controller/PhotoController.ts
+++ src/controller/PhotoController.ts
@@ -1,135 +1,152 @@
-import { NextFunction, Request, Response } from 'express'
-import { AppDataSource } from '../data-source'
-import { Photo } from '../entity/Photo'
-import { User } from '../entity/User'
-import { ERROR_MESSAGES } from '../utils/errorMessages'
+import { NextFunction, Request, Response } from 'express';
+import { AppDataSource } from '../data-source';
+import { Photo } from '../entity/Photo';
+import { User } from '../entity/User';
+import { ERROR_MESSAGES } from '../utils/errorMessages';
 
 export class PhotoController {
   static createNewPhoto = async (
     req: Request,
-    res: Response
+    res: Response,
   ): Response<Photo> => {
     try {
-      const ur = AppDataSource.getRepository(User)
+      const ur = AppDataSource.getRepository(User);
       // get user.id
-      const userId = req.user.userId
+      const userId = req.user.userId;
 
-      const user = await ur.findOne({
+      const user = await ur.findOneOrFail({
         relations: ['photos'],
         where: {
-          id: userId, // where the id is of the author. expecting three 3 photos connected to this single author
+          id: userId,
         },
-      })
-
-      // todo: wrap in try/catch and instead of findOne should be findOneOrFail
-      if (user) {
-        console.log('User found!')
-        console.log(user)
-      }
+      });
 
       // Extract photo payload from req.body
-      const { name, description, filename, views, isPublished } = req.body
+      const { name, description, filename, views, isPublished } = req.body;
 
       // Add some photos
-      const photo = new Photo()
-      ;(photo.name = name),
-        (photo.description = description),
-        (photo.filename = filename),
-        (photo.views = views),
-        (photo.isPublished = isPublished)
+      const photo = new Photo();
+      photo.name = name;
+      photo.description = description;
+      photo.filename = filename;
+      photo.views = views;
+      photo.isPublished = isPublished;
 
-      user.photos = [...user.photos, photo]
+      user.photos = [...user.photos, photo];
 
       // TODO: create userDTO for User entity
       // update user.photos
-      ur.save(user)
+      ur.save(user);
 
-      return res.status(200).send(photo)
+      return res.status(200).send(photo);
     } catch (error) {
       return res.status(404).send({
         error: {
           message: ERROR_MESSAGES.REQUEST_NOT_FOUND,
         },
-      })
+      });
     }
-  }
+  };
 
   static getAllPhotos = async (
     req: Request,
-    res: Response
+    res: Response,
   ): Response<Photo[]> => {
     try {
-      const ur = AppDataSource.getRepository(User)
+      const ur = AppDataSource.getRepository(User);
       // get user.id
-      const userId = req.user.userId
+      const userId = req.user.userId;
       const user = await ur.findOne({
         relations: ['photos'],
         where: {
           id: userId, // where the id is of the author. expecting three 3 photos connected to this single author
         },
-      })
+      });
 
-      return res.status(200).send(user)
+      return res.status(200).send(user);
     } catch (error) {
       return res.status(404).send({
         error: {
           message: ERROR_MESSAGES.REQUEST_NOT_FOUND,
         },
-      })
+      });
     }
-  }
+  };
 
-  static update = async (req: Request, res: Response): Response<any> => {
+  static updateDescription = async (
+    req: Request,
+    res: Response,
+  ): Response<any> => {
     try {
-      const ur = AppDataSource.getRepository(User)
-      const userId = req.user.userId // Get the user ID from previous midleware
+      const ur = AppDataSource.getRepository(User);
+      const userId = req.user.userId; // Get the user ID from previous midleware
 
       const user = await ur.findOne({
         relations: ['photos'],
         where: {
           id: userId,
         },
-      })
+      });
 
       // Retrieve the Photo entity with ID 2 from the user's photos array
       const updatedPhoto = user.photos.find(
-        (photo) => photo.id === Number(req.params.id)
-      ) // todo extract req.body.id from req.params
-      updatedPhoto.description = req.body.description
+        (photo) => photo.id === Number(req.params.id),
+      ); // todo extract req.body.id from req.params
+
+      updatedPhoto.description = req.body.description;
 
       // Save the updated Photo entity to the database
-      await ur.save(user)
-      res.status(200).send(updatedPhoto)
+      await ur.save(user);
+      res.status(200).send(updatedPhoto);
     } catch (error) {
       return res.status(404).send({
         error: {
           message: ERROR_MESSAGES.REQUEST_NOT_FOUND,
         },
-      })
+      });
     }
-  }
+  };
+
+  static updatePartial = async (req: Request, res: Response): Response<any> => {
+    try {
+      const qb = AppDataSource.getRepository(Photo).createQueryBuilder();
+      const updatedPhotoPayloadObj: Partial<Photo> = req.body;
+
+      if (Object.keys(updatedPhotoPayloadObj).length !== 0) {
+        await qb
+          .update(Photo)
+          .set({ ...updatedPhotoPayloadObj })
+          .where('id = :id', { id: req.params.photoId })
+          .execute();
+      }
+
+      res.status(200).send(updatedPhotoPayloadObj);
+    } catch (error) {
+      return res.status(404).send(error);
+    }
+  };
 
   static delete = async (req: Request, res: Response): Response<any> => {
     try {
-      const pr = AppDataSource.getRepository(Photo)
+      const pr = AppDataSource.getRepository(Photo);
       // Find the photo we want to delete
       const photo = await pr.findOneOrFail({
         relations: ['user'],
         where: {
           id: req.params.photoId,
         },
-      })
+      });
 
       // Delete the photo from the DB
-      await pr.delete(photo)
+      await pr.delete(photo);
 
-      return res.status(200).send('OK!')
+      return res.status(200).send('OK!');
     } catch (error) {
       return res.status(404).send({
         error: {
           message: ERROR_MESSAGES.REQUEST_NOT_FOUND,
         },
-      })
+      });
     }
-  }
+  };
 }
--- src/routes/photo.ts
+++ src/routes/photo.ts
@@ -1,28 +1,33 @@
-import { Router } from 'express'
-import { PhotoController } from '../controller/PhotoController'
-import { checkJWT } from '../middleware/checkJWT'
-import { checkRole } from '../middleware/checkRole'
+import { Router } from 'express';
+import { PhotoController } from '../controller/PhotoController';
+import { checkJWT } from '../middleware/checkJWT';
+import { checkRole } from '../middleware/checkRole';
 
-const routes: Router = Router()
+const routes: Router = Router();
 routes.get(
   '/new',
   [checkJWT, checkRole(['ADMIN'])],
-  PhotoController.createNewPhoto
-)
+  PhotoController.createNewPhoto,
+);
 routes.get(
   '/getAll',
   [checkJWT, checkRole(['ADMIN'])],
-  PhotoController.getAllPhotos
-)
+  PhotoController.getAllPhotos,
+);
 routes.put(
   '/update/:id',
   [checkJWT, checkRole(['ADMIN'])],
-  PhotoController.update
-)
+  PhotoController.updateDescription,
+);
+routes.patch(
+  '/update/:photoId',
+  [checkJWT, checkRole(['ADMIN'])],
+  PhotoController.updatePartial,
+);
 routes.delete(
   '/delete/:photoId',
   [checkJWT, checkRole(['ADMIN'])],
-  PhotoController.delete
-)
+  PhotoController.delete,
+);
 
-export default routes
+export default routes;
